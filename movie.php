40ad8b46e1a8db76e16f67ad106550640fce4baa
Kazami <kazamizero@gmail.com>
2016-03-11 23:55:17 +0800

    title: framework initial

---
+++ movie.php
@@ -0,0 +1,10 @@
+<?php
+echo "<!DOCTYPE HTML>";
+echo "<html>";
+echo "<head>";
+echo "</head>";
+echo "<body>";
+echo "<video control loop autoplay> <source src=\"http://az836905.vo.msecnd.net/videos/f97c5bd064ae47dfe95f8cfb1fbb0356.mp4\"/></video>";
+echo "</body>";
+
+echo "</html>";
---
+++ src/pages/hello.php
@@ -0,0 +1,2 @@
+<?php echo $name; ?>
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
