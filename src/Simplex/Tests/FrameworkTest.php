6c30a2127dc430c2ba08eb38c052cb224921bd3e
Kazami <kazamizero@gmail.com>
2016-03-15 11:32:27 +0800

    title: test case for framework

---
+++ src/Simplex/Tests/FrameworkTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Simplex\Tests;
+
+use Simplex\Framework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
+
+class FrameworkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNotFoundHandling()
+    {
+        $framework = $this->getFrameworkForException(new ResourceNotFoundException());
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    private function getFrameworkForException($exception)
+    {
+        $matcher = $this->getMock('Symfony\Component\Routing\Matcher\UrlMatcherInterface');
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->throwException($exception));
+
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->getMock('Symfony\Component\Routing\RequestContext')));
+        $resolver = $this->getMock('Symfony\Component\HttpKernel\Controller\ControllerResolverInterface');
+
+        return new Framework($matcher, $resolver);
+    }
+
+    public function testErrorHandling()
+    {
+        $framework = $this->getFrameworkForException(new \RuntimeException());
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(500, $response->getStatusCode());
+    }
+
+}
