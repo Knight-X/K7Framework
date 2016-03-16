dc2ecb4b749b2e8a72cd453ac23197b17f718c69
Kazami <kazamizero@gmail.com>
2016-03-16 12:09:34 +0800

    title: front php edited what: add event listen in the front php

--- web/front.php
+++ web/front.php
@@ -4,26 +4,24 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
+    $response = $event->getResponse();
 
-function render_template($request) 
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    if ($response->isRedirection()
+        || ($response->headers->has('Content-Type') && false == strpos($response->headers->get('Content-Type'), 'html'))
+        || 'html' !== $event->getRequest()->getRequestFormat())
+    {
+        return;
+    }
 
-    return new Response(ob_get_clean());
-}
+    $response->setContent($response->getContent().'GA CODE');
+});
 
-$request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
-
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$framework = new Simplex\Framework($matcher, $resolver);
+$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
 $response = $framework->handle($request);
 $response->send();
